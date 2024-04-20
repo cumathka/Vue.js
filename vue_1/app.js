ac2f9b23200af9e3b84aa126cf138962b37e97d6
cumathka <cumakaya2531@gmail.com>
2024-04-20 21:19:29 +0200

    input ok

--- vue_1/app.js
+++ vue_1/app.js
@@ -23,7 +23,7 @@ const app = Vue.createApp({
   },
   methods: {
     addGoal() {
-      this.goals.push(this.enteredValue); 
+      this.goals.push(this.enteredValue);
       this.enteredValue = "";
     },
   },
