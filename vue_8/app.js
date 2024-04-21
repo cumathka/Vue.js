3cce731b3ca6e5fdb9043df79098a03d2ebb4fd6
cumathka <cumakaya2531@gmail.com>
2024-04-21 22:36:26 +0200

    event modifies ok

---
+++ vue_8/app.js
@@ -0,0 +1,34 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            counter : 0 ,
+            name : '' ,
+            confirmedName : ''
+        }
+    },
+    methods: {
+        confirmInput(){
+        this.confirmedName = this.name ;
+        },
+        submitForm(event){
+           // event.preventDefault(); // prevent form submission 1. way 
+           // 2.way   <form v-on:submit.prevent="submitForm"> in html
+            alert("submited")
+
+        },
+        setName(event,lastName){
+            this.name = event.target.value + ' ' + lastName;
+        },
+        add(num){
+            this.counter = this.counter + num ;
+        },
+
+        reduce(num){
+            this.counter = this.counter - num ;
+        }
+    },
+})
+
+
+
+app.mount("#events");
