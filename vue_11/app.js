0c7c2c09b7d0bd3532bdadaac965e375e0a5f40d
cumathka <cumakaya2531@gmail.com>
2024-04-23 16:02:26 +0200

    remove goal ok

---
+++ vue_11/app.js
@@ -0,0 +1,26 @@
+
+const app = Vue.createApp({
+    data (){
+        return {
+            enteredValue : "",
+            goals : [],
+        }
+    },
+
+    methods: {
+        addGoal(){
+            this.goals.push(this.enteredValue)
+            console.log(this.goals);
+            this.enteredValue = "";
+        },
+        removeGoal(index){
+            this.goals.splice(index,1)
+        }
+
+    },
+    
+})
+
+
+
+app.mount("#user-goals")
